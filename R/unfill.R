385c3d9ece8c958530351d6e05db3af86b57e556
Mikko Marttila <13412395+mikmart@users.noreply.github.com>
2023-05-01 20:20:31 +0100

    Refine unfill() docs

--- R/unfill.R
+++ R/unfill.R
@@ -4,14 +4,14 @@
 #' @param value A replacement value of length 1.
 #'
 #' @returns The vector `x` with adjacent repeated elements replaced with `value`.
-#' @seealso [rle()] to find runs of repeated elements.
 #' @seealso [tidyr::fill()] and [vctrs::vec_fill_missing()] for the reverse operation.
+#' @seealso [rle()] to find runs of repeated elements.
 #'
 #' @examples
 #' unfill(rep(1:3, 2:4))
 #' unfill(rep(c("A", "B"), 3:4), "")
 #'
-#' # Unfilling data frames can clarify grouping in a quick display
+#' # Unfilling can clarify row grouping in a quick display
 #' x <- expand_grid(
 #'   foo = c("A", "B"),
 #'   bar = c("a", "b", "c"),
@@ -21,6 +21,6 @@
 #' x
 #' @export
 unfill <- function(x, value = NA) {
-  stopifnot(length(value) == 1)
-  replace(x, c(FALSE, head(x, -1) == tail(x, -1)), value)
+  stopifnot(length(value) == 1L)
+  replace(x, c(FALSE, head(x, -1L) == tail(x, -1L)), value)
 }
