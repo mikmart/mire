b93467e83161ee8903713160c9c60a1c48b06a7a
Mikko Marttila <13412395+mikmart@users.noreply.github.com>
2023-05-01 19:07:54 +0100

    Document unfill()

--- R/unfill.R
+++ R/unfill.R
@@ -1,4 +1,25 @@
+#' Replace repeated elements in a vector
+#'
+#' @param x A vector.
+#' @param value A replacement value of length 1.
+#'
+#' @returns The vector `x` with adjacent repeated elements replaced with `value`.
+#' @seealso [rle()] to find runs of repeated elements.
+#'
+#' @examples
+#' unfill(rep(1:3, 2:4))
+#' unfill(rep(c("A", "B"), 3:4), "")
+#'
+#' # Unfilling data frames can clarify grouping in a quick display
+#' x <- expand_grid(
+#'   foo = c("A", "B"),
+#'   bar = c("a", "b", "c"),
+#'   baz = c(1, 2)
+#' )
+#' x[] <- map(unfill, x, "")
+#' x
 #' @export
-unfill <- function(x) {
-  replace(x, c(FALSE, head(x, -1) == tail(x, -1)), NA)
+unfill <- function(x, value = NA) {
+  stopifnot(length(value) == 1)
+  replace(x, c(FALSE, head(x, -1) == tail(x, -1)), value)
 }
