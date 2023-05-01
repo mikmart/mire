5ed134a9a39107b00b014d3a6d69c192e6f6343a
Mikko Marttila <13412395+mikmart@users.noreply.github.com>
2023-05-01 20:39:10 +0100

    Document resample(), prune shuffle()

--- R/resample.R
+++ R/resample.R
@@ -1,9 +1,14 @@
+#' Sample a vector with replacement
+#'
+#' @inheritParams base::sample
+#' @seealso [sample()] which this (extremely) thinly wraps.
+#'
+#' @returns A vector of length `size` with elements drawn from `x` or---if `x`
+#'   is a positive number with length 1---from the integers `1:x`.
+#'
+#' @examples
+#' resample(1:5)
 #' @export
 resample <- function(x, size, prob = NULL) {
-  sample(x, size, replace = TRUE, prob)
-}
-
-#' @export
-shuffle <- function(x) {
-  x[sample(length(x))]
+  sample(x, size, replace = TRUE, prob = prob)
 }
