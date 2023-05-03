923db9720c5b265267adb8d54ca78f01eb53627f
Mikko Marttila <13412395+mikmart@users.noreply.github.com>
2023-05-03 22:51:55 +0100

    Clarify code

--- src/let.c
+++ src/let.c
@@ -1,13 +1,8 @@
 #define R_NO_REMAP
 #include <Rinternals.h>
 
-extern SEXP R_ExprSymbol;
-
-Rboolean is_pairlist_all_named(SEXP x) {
-  for (; x != R_NilValue; x = CDR(x))
-    if (TAG(x) == R_NilValue) return FALSE;
-  return TRUE;
-}
+extern SEXP R_ExprSymbol; // = Rf_install("expr") on load
+static Rboolean all_nodes_in_pairlist_have_tags(SEXP);
 
 SEXP ffi_let(SEXP call, SEXP op, SEXP args, SEXP rho) {
   SEXP expr = Rf_findVar(R_ExprSymbol, rho);
@@ -17,7 +12,7 @@ SEXP ffi_let(SEXP call, SEXP op, SEXP args, SEXP rho) {
     return R_NilValue;
   if (dots == R_MissingArg)
     return Rf_eval(PREXPR(expr), PRENV(expr));
-  if (!is_pairlist_all_named(dots))
+  if (!all_nodes_in_pairlist_have_tags(dots))
     Rf_error("All arguments in `...` must be named.");
 
   SEXP env = PROTECT(R_NewEnv(PRENV(expr), 0, 0));
@@ -26,4 +21,10 @@ SEXP ffi_let(SEXP call, SEXP op, SEXP args, SEXP rho) {
 
   UNPROTECT(1);
   return Rf_eval(PREXPR(expr), env);
+}
+
+Rboolean all_nodes_in_pairlist_have_tags(SEXP x) {
+  for (; x != R_NilValue; x = CDR(x))
+    if (TAG(x) == R_NilValue) return FALSE;
+  return TRUE;
 }
