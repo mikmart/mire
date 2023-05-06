068d23a1897a0a3c0e549d65d5c380847e8918d1
Mikko Marttila <13412395+mikmart@users.noreply.github.com>
2023-05-06 14:18:20 +0100

    Evaluate PRCODE directly

--- src/let.c
+++ src/let.c
@@ -5,22 +5,22 @@ extern SEXP R_ExprSymbol; // = Rf_install("expr") on load
 static Rboolean all_nodes_in_pairlist_have_tags(SEXP);
 
 SEXP ffi_let(SEXP call, SEXP op, SEXP args, SEXP rho) {
-  SEXP expr = Rf_findVar(R_ExprSymbol, rho);
-  SEXP dots = Rf_findVar(R_DotsSymbol, rho);
+  SEXP expr = Rf_findVarInFrame(rho, R_ExprSymbol);
+  SEXP dots = Rf_findVarInFrame(rho, R_DotsSymbol);
 
   if (expr == R_MissingArg)
     return R_NilValue;
   if (dots == R_MissingArg)
-    return Rf_eval(PREXPR(expr), PRENV(expr));
+    return Rf_eval(PRCODE(expr), PRENV(expr));
   if (!all_nodes_in_pairlist_have_tags(dots))
     Rf_error("All arguments in `...` must be named.");
 
   SEXP env = PROTECT(R_NewEnv(PRENV(expr), 0, 0));
   for (SEXP cons = dots; cons != R_NilValue; cons = CDR(cons))
-    Rf_defineVar(TAG(cons), Rf_eval(PREXPR(CAR(cons)), env), env);
+    Rf_defineVar(TAG(cons), Rf_eval(PRCODE(CAR(cons)), env), env);
 
   UNPROTECT(1);
-  return Rf_eval(PREXPR(expr), env);
+  return Rf_eval(PRCODE(expr), env);
 }
 
 Rboolean all_nodes_in_pairlist_have_tags(SEXP x) {
