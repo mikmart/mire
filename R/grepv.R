9acf52da4d1bae9a454f2ec57b1a7a52313e2b99
Mikko Marttila <13412395+mikmart@users.noreply.github.com>
2023-05-20 12:18:02 +0100

    Prune more functions

--- R/grepv.R
+++
@@ -1,4 +0,0 @@
-#' @export
-grepv <- function(pattern, x, ignore.case = FALSE, perl = FALSE, fixed = FALSE, useBytes = FALSE, invert = FALSE) {
-  grep(pattern, x, ignore.case, perl, value = TRUE, fixed, useBytes, invert)
-}
