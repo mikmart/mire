bdb3e708b73283a642af7b1b0e044ee350dca0b0
Mikko Marttila <13412395+mikmart@users.noreply.github.com>
2023-11-02 02:40:16 +0000

    Use infix operator in tests

--- tests/testthat/test-matpow.R
+++ tests/testthat/test-matpow.R
@@ -1,24 +1,21 @@
 test_that("raising to a power works", {
   X <- matrix(1:4, 2, 2)
-  expect_equal(matpow(X, 1), X)
-  expect_equal(matpow(X, 2), X %*% X)
-  expect_equal(matpow(X, 3), X %*% X %*% X)
+  expect_equal(X %^% 1, X)
+  expect_equal(X %^% 2, X %*% X)
+  expect_equal(X %^% 3, X %*% X %*% X)
 })
 
 test_that("power of zero returns identity", {
-  X <- matrix(1:4, 2, 2)
-  I <- diag(2)
-  expect_equal(matpow(X, 0), I)
+  expect_equal(matrix(1:4, 2) %^% 0, diag(2))
 })
 
 test_that("negative powers invert", {
   X <- matrix(1:4, 2, 2)
   Xinv <- solve(X)
-  expect_equal(matpow(X, -1), Xinv)
-  expect_equal(matpow(X, -2), Xinv %*% Xinv)
+  expect_equal(X %^% -1, Xinv)
+  expect_equal(X %^% -2, Xinv %*% Xinv)
 })
 
 test_that("non-integer powers signal error", {
-  X <- matrix(1:4, 2, 2)
-  expect_error(matpow(X, 0.5))
+  expect_error(matrix(1:4, 2, 2) %^% 0.5)
 })
