f8a809d6244680750a524c52fd524ead63004fc1
Mikko Marttila <13412395+mikmart@users.noreply.github.com>
2024-03-10 10:31:50 +0000

    Remove mapsome() function
    I'm not convinced about the name or accepting `...`

--- R/mapsome.R
+++ R/mapsome.R
@@ -9,25 +9,14 @@
 #'
 #' @param x An object.
 #' @param f A function.
-#' @param ... Additional arguments passed to `f`.
 #'
-#' @returns `NULL` if `x` is null. Otherwise the result of `f(x, ...)`.
+#' @returns `NULL` if `x` is null. Otherwise the result of `f(x)`.
 #'
 #' @examples
 #' try(strsplit(NULL, ""))
 #' NULL %>>% \(x) strsplit(x, "")
 #' "42" %>>% \(x) strsplit(x, "")
-#'
-#' # The pipe-friendly function form.
-#' NULL |> mapsome(strsplit, "")
-#' @name mapsome
 #' @export
 `%>>%` <- function(x, f) {
   if (is.null(x)) x else f(x)
-}
-
-#' @rdname mapsome
-#' @export
-mapsome <- function(x, f, ...) {
-  if (is.null(x)) x else f(x, ...)
 }
